1a8ed666fdcc18def650b558f2073d424e7120df
Avinash_Arex <87872686+avinasharex@users.noreply.github.com>
2024-01-08 14:58:18 +0000

    write updateCollection controller

--- src/controllers/collection.controller.js
+++ src/controllers/collection.controller.js
@@ -1,11 +1,11 @@
-import Collection  from "../models/collection.model.js";
+import Collection from "../models/collection.model.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-export const createCollection = asyncHandler(async(req,res)=>{
-    const {name} = req.body
+export const createCollection = asyncHandler(async (req, res) => {
+    const { name } = req.body
 
-    if(!name){
+    if (!name) {
         throw new ApiError("Collection name is required", 401)
     }
 
@@ -17,5 +17,31 @@ export const createCollection = asyncHandler(async(req,res)=>{
         success: true,
         message: "Collection created successfully",
         collection
+    })
+})
+
+export const updateCollection = asyncHandler(async (req, res) => {
+    const { name } = req.body
+    const { collectionId } = req.params
+
+    if (!name) {
+        throw new ApiError("Collection name is required", 401)
+    }
+
+    const updatedCollection = await Collection.findByIdAndUpdate(collectionId, {
+        name
+    }, {
+        new: true,
+        runValidators: true
+    })
+
+    if(!updatedCollection){
+        throw new ApiError("Collection not found", 401)
+    }
+
+    return res.status(200).json({
+        success: true,
+        message: "Collection updated successfully",
+        updatedCollection
     })
 })
